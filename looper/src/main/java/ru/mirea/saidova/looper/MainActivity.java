56a09f5e90bba5f68c667b60e8eaac511cc1945c
kami-sa <kamisa1999@yandex.ru>
2019-11-17 14:48:41 +0300

    Task 3.2 second part add

--- looper/src/main/java/ru/mirea/saidova/looper/MainActivity.java
+++ looper/src/main/java/ru/mirea/saidova/looper/MainActivity.java
@@ -2,21 +2,27 @@ package ru.mirea.saidova.looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Message;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     MyLooper myLooper;
+    EditText editText;
+    TextView textView;
+//    int myAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText  = (EditText) findViewById(R.id.editText);
+        textView  = (TextView) findViewById(R.id.textView);
         myLooper = new MyLooper();
         myLooper.start();
+
     }
     public void onClick(View view){
         Message msg = new Message();
@@ -26,5 +32,11 @@ public class MainActivity extends AppCompatActivity {
         if (myLooper != null) {
             myLooper.handler.sendMessage(msg);
         }
+    }
+
+    public void btnClick(View view){
+        Double myAge = Double.parseDouble(editText.getText().toString());
+        Double newAge = myAge + 40;
+        textView.setText(newAge.toString());
     }
 }
